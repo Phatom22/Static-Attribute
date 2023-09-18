b70e7d45d71a3d26d51c5a25cf8ce825f72c7dc7
nomvula <nzshabangu95@gmail.com>
2023-09-18 11:01:28 +0200

    Add project files.

---
+++ Static Attribute/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs song1 = new Songs("holiday", "green day" ,115);
+            Songs song2 = new Songs("billy", "jean", 200);
+            Console.WriteLine(song1.title);
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Static Attribute/Songs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    class Songs
+    {
+        public string title;
+            public string artist;
+        public int duration;
+
+        public static int songCount;
+            public Songs(string aTitle, string aArtist, int aDuration) {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+
+        }
+    }
+}
